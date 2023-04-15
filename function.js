bfd3ec7f3d47ca018c94b8f3848fef1b139b905d
devzuw <130874828+devzuw@users.noreply.github.com>
2023-04-15 19:51:12 -0400

    Add files via upload
    *codigo

---
+++ function.js
@@ -0,0 +1,34 @@
+//coger elementos del Doom
+const resultInput = document.getElementById('result');
+const buttons = document.querySelectorAll('.elemnts');
+
+// funcion
+buttons.forEach(button => {
+  button.addEventListener('click', () => { //"escuchar" los clicks sobre los botones númericos
+    event.preventDefault(); // evitar que la pagina se actulize al enviar el formulario
+    const buttonValue = button.textContent; // esta const se encarga de almazenar el contenido de button
+    let newValue; //  almasenamiento de todo
+
+    if (buttonValue === 'CE') {
+            // boton de borrar todo
+      newValue = '';
+    } else if (buttonValue === 'C' || buttonValue === ' ') {
+      //boton de borrar digíto por digíto
+      newValue = resultInput.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+      // evaluacion y ejecucion de operaciones
+      let expression = resultInput.value;
+      expression = expression.replace('x', '*');
+      expression = expression.replace('÷', '/');
+      expression = expression.replace('REST', '%');
+      const result = eval(expression); // eval se encarga de evaluar cada expresion y ejecutarla, por esto "=" se define como resultado de manera automatica
+      newValue = result; // datos almacenados de expresion
+    } else {
+      //agrega los datos
+      newValue = resultInput.value + buttonValue;
+    }
+
+    // Actualizar el valor del campo de entrada
+    resultInput.value = newValue;
+  });
+});
